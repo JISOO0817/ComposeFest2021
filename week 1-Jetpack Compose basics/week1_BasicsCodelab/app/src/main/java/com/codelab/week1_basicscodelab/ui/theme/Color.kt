e23b44e5f16042e419a163e7620f2bfbc41b2ad2
JISOO0817 <dkadnsk@naver.com>
2021-12-01 17:26:10 +0900

    test

---
+++ week 1-Jetpack Compose basics/week1_BasicsCodelab/app/src/main/java/com/codelab/week1_basicscodelab/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.codelab.week1_basicscodelab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val HotPink = Color(0xFFF13374)
+val Pink = Color(0xFFEE98B5)
+val LightPink = Color(0xFFFDD9E5)
+val Chartreuse = Color(0xFFEFF7CF)
