78035695bab090ad8a7c71f72eb5f1455a39d403
JISOO0817 <dkadnsk@naver.com>
2021-12-08 16:11:55 +0900

    - compose 에서의 viewmodel 사용

---
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModelTest.kt
@@ -0,0 +1,21 @@
+package com.codelabs.state.todo
+
+import com.codelabs.state.util.generateRandomTodoItem
+
+class TodoViewModelTest {
+//    @Test
+//    fun whenRemovingItem_updatesList() {
+//
+//        val viewModel = TodoViewModel()
+//        val item1 = generateRandomTodoItem()
+//        val item2 = generateRandomTodoItem()
+//        viewModel.addItem(item1)
+//        viewModel.addItem(item2)
+//
+//        viewModel.removeItem(item1)
+//
+//        assertThat(viewModel.todoItems).isEqualTo(listOf(item2))
+//    }
+
+
+}
